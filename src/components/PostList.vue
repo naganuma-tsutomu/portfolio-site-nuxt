346e208a7cf3870ec3dc577ed787bc5717055a45
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2023-12-19 15:06:56 +0900

    update: ページネーション

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,3 @@
-<script setup>
-const config = useRuntimeConfig();
-const { data: posts, error } = await useFetch(
-  `${config.public.WPAPI}/wp-json/wp/v2/tourist-spot/?acf_format=standard`
-);
-</script>
 <template>
   <div class="ml-5 mr-5">
     <h1 class="m-auto font-bold text-xl text-center mt-2 mb-2">観光地記事</h1>
@@ -20,5 +14,65 @@ const { data: posts, error } = await useFetch(
     <div v-else-if="error">
       <p>エラーが発生しました</p>
     </div>
+    <div>
+      <div v-if="currentPage !== '1'">
+        <NuxtLink
+          :to="updateRoute(parseInt(currentPage) - 1)"
+          >Previous</NuxtLink
+        >
+      </div>
+      <div v-if="currentPage !== totalPages">
+      <NuxtLink
+        :to="updateRoute(parseInt(currentPage) + 1)"
+      >
+        Next
+      </NuxtLink>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+const route = useRoute();
+watch(
+  () => route.query,
+  () => {
+    currentPage.value = route.query.page;
+  }
+);
+const currentPage = ref(route.query.page || "1"); // 現在のページ番号
+const perPage = 3; // 1ページに表示する記事の数
+const {
+  data: posts,
+  pending,
+  refresh,
+  error,
+} = await useAsyncData(
+  "post-data",
+  () =>
+    $fetch(
+      `${config.public.WPAPI}/wp-json/wp/v2/tourist-spot/?acf_format=standard&page=${currentPage.value}&per_page=${perPage}`,
+      {
+        params: {
+          page: currentPage.value,
+        },
+      }
+    ),
+  {
+    watch: [currentPage],
+  }
+);
+const totalPages = computed(() => {
+  // トータルページ数の計算方法はデータに基づいて調整する必要があります
+  // ここでは簡略化して10ページとしています
+  return '10';
+});
+const updateRoute = (page) => {
+  return {
+    path: route.path,
+    query: { page: page },
+  };
+};
+</script>
--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -1,5 +1,26 @@
+<script setup>
+  const config = useRuntimeConfig();
+  const { data, pending, error, refresh } = await useFetch(`${config.public.WPAPI}/wp-json/wp/v2/tourist-spot/?acf_format=standard`);
+
+
+</script>
 <template>
   <div>
-    <NuxtLink to="/products/item"><p>アイテム</p></NuxtLink>
+    <p v-if="error">{{ error }}</p>
+    <button @click="refresh()">再取得</button>
+    <table border="1">
+      <thead>
+        <tr><th>ID</th><th>Title</th><th>Summary</th><th>Created by</th><th>Created at</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="article in data?.articles" :key="article.id">
+          <td>{{ article.id }}</td>
+          <td>{{ article.title }}</td>
+          <td>{{ article.summary }}</td>
+          <td>{{ article.created_by }}</td>
+          <td>{{ article.created_at }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
