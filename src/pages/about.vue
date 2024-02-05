8a17a28e7732fc3648ce486bf6c99503f4e2af30
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2024-02-05 18:02:25 +0900

    update: about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,67 @@
 <template>
-  <div>
-    <a href="/about">
-        <p>こん</p>
-    </a>
+  <div class="mt-16">
+    <h1 class="text-4xl mb-4 font-bold indent-3">ABOUT</h1>
+    <div>
+      <div class="">
+        <img
+          class="h-40 rounded-lg mt-8 mb-4"
+          src="../assets/img/top-img.jpg"
+          alt="画像"
+        />
+        <h3 class="font-bold text-xl">NAGANUMA</h3>
+        <p class="mt-2">web engineer in Fukushima</p>
+        <p class="mt-6">福島県でWEBエンジニアをしています。</p>
+        <p>
+          主にWordpressやnuxt.jsを使用してホームページの制作や、VBAでの自動処理プログラムの作成を行っています。
+        </p>
+        <p>趣味は料理です。</p>
+      </div>
+      <div class="mt-10">
+        <h2 class="text-3xl text-bold indent-3">SKILL</h2>
+        <div class="grid grid-cols-2 gap-3 mt-4">
+          <div class="">
+            <p class="text-xl mb-2">Language</p>
+            <div class="[&_span]:block [&_span]:indent-5">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>TypeScript</span>
+              <span>PHP</span>
+              <span>VBA</span>
+            </div>
+          </div>
+          <div>
+            <p class="text-xl mb-2">Framework</p>
+            <div class="[&_span]:block [&_span]:indent-5">
+              <span>Bootstrap</span>
+              <span>Tailwind CSS</span>
+              <span>jQuery</span>
+              <span>Vue.js</span>
+              <span>Nuxt.js</span>
+              <span>React</span>
+              <span>Next.js</span>
+              <span>Laravel</span>
+            </div>
+          </div>
+          <div>
+            <p class="text-xl mb-2">CMS</p>
+            <div class="[&_span]:block [&_span]:indent-5">
+              <span>WordPress</span>
+              <span>Newt</span>
+            </div>
+          </div>
+          <div>
+            <p class="text-xl mb-2">Other</p>
+            <div class="[&_span]:block [&_span]:indent-5">
+              <span>Visual Studio Code</span>
+              <span>Node.js</span>
+              <span>Git</span>
+              <span>GitHub</span>
+              <span>Docker</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
