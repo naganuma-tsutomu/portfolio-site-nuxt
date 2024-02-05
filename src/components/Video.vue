c503671a33f4b2c519e88ad3412ecbd5157a50db
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2024-02-05 18:08:36 +0900

    update

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
   <div class="fixed h-[100vh] w-[100vw] top-0 left-0 overflow-hidden -z-10 after:content-[''] bg-[url('../img/image.webp')] bg-black bg-center bg-cover after:w-full after:h-full after:opacity-50
-=======
-  <div class="fixed h-[100vh] w-[100vw] top-0 left-0 overflow-hidden -z-10 after:content-[''] bg-[url('../img/image.webp')] after:w-full after:h-full after:opacity-50
->>>>>>> b24f46bcca210b27442a1984906f9b30512e97b7
     after:bg-black after:block after:absolute after:top-0 after:left-0">
     <video class="absolute min-h-full min-w-full max-w-none top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" autoplay loop muted playsinline>
       <source src="../assets/video/video-m.mov" />
