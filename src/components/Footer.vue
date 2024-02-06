4a00024025ce8155afe29261cdaa6b6a5d638798
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2024-02-06 15:22:33 +0900

    update: github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="h-[7rem] grid items-center">
     <div class="flex justify-center">
-      <a href="">
+      <a href="https://github.com/naganuma-tsutomu">
         <img class="w-8 h-8" src="../assets/img/github-mark-white.svg" alt="github" />
       </a>
     </div>
